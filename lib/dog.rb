524f9532c2e9549ce65288ea7b56e696ca8c90df
Fer Hernandez <fh.1111@gmail.com>
2018-10-07 13:46:02 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog 
+
+
+    def initialize(name)
+      @name = name
+    end
+   
+    def name
+      @name
+    end
+   
+    def name=(new_name)
+      @name = new_name
+    end
+  
+
+end
+
+
+max = Dog.new("Max")
+puts "#{max.name}"
+max.name = "Max2"
+puts "#{max.name}"
